f20a6f8655a1655aba9f65527b6475fce12ebb23
pateriyadeepali <pateriyadeepali1008@gmail.com>
2025-11-17 12:32:14 +0530

    new

--- lib/config/dev_config.dart
+++ lib/config/dev_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 final environmentConfig = {
-  "title": "Flutter Demo - DEV",
+  "title": "Flutter Demo - DEVeloper",
   "primaryColor": 0xFFFF0000,      // Red
   "fabColor": 0xFFFF5252,          // Red Accent
   "bgColor": 0xFFFFFFFF,
